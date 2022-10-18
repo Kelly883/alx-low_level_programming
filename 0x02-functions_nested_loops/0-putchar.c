ca6f0d653fb5e6fa69c49899f14ee65264f2f2dc
kelly883 <emekelechi883@gmail.com>
2022-10-18 09:11:32 -0700

    my task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - will print _putchar
+ *Return:0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+-putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
