1554de9cb107fa5a8665c5c9511a568ed1070721
kelly883 <emekelechi883@gmail.com>
2022-10-20 13:57:03 -0700

    my tasks

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - number more
+ * Return: return nothing
+ * this is nothing
+ */
+
+void more_numbers(void)
+{
+
+int a, c;
+	for (a = 1; a <= 10; a++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+			_putchar('2');
+			_putchar(c % 10 + '0');
+		}
+			_putchar('\n');
+		}
+}
