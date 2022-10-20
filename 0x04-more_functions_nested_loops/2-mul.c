57e4445130f86a9b351aa14a5082908c4367f7fb
kelly883 <emekelechi883@gmail.com>
2022-10-20 12:45:31 -0700

    first3 tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @d: first integer
+* @c: second integer
+* Return: addition of d and c
+*/
+int mul(int d, int c)
+{
+	return (d * c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_numbers - print from 0 to 9
+ *Return:void
+ */
+void print_numbers(void)
+{
+	char b;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+	_putchar(b);
+	}
+	_putchar('\n');
+}
