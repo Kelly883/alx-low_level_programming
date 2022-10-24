cf9262c07d76a83826af9b0ba7a85dba50da6417
kelly883 <emekelechi883@gmail.com>
2022-10-24 13:26:31 -0700

    task 1 to 5 commited

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length.of a string.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != 0; x++)
+	{
+	}
+	return (x);
+}
