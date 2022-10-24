09c3456eca92fc67b57d90a2b70fce5e1a247505
kelly883 <emekelechi883@gmail.com>
2022-10-24 13:10:08 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: declaration of n and parameters for the functions
+*  Return: 0 is success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
