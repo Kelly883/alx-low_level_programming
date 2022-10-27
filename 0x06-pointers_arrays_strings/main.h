db718903cdb9a5e35a7fcdbf1652f4fd49ac6695
kelly883 <emekelechi883@gmail.com>
2022-10-27 13:08:35 -0700

    task 3

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char b);
+ int _putchar(char b);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,4 +10,3 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 #endif
-	;
