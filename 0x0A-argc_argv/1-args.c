bb3154e7b79aedb7fded15e0037331e5b139e0cd
kelly883 <emekelechi883@gmail.com>
2022-11-04 08:21:38 -0700

    task 2 and 3

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
 /**
- * main - prints all arguments it receives.
- * @argc: argument count
- * @argv: arguments
- *
+ * main - program that prints its name, followed by a new line
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-
+	if (argc)
+	{
+		printf("%d\n", argc - 1);
+		(void)argv;
+	}
 	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - multiplies two numbers.
- * @argc: argument count
- * @argv: arguments
- *
+ * main - program that prints its name, followed by a new line
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int x, y;
+	int i;
 
-	if (argc < 3)
+	for (i = 0; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		printf("%s\n", argv[i]);
 	}
-
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	printf("%d\n", x * y);
-
 	return (0);
 }
