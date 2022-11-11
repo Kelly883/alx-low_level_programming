75861ff145ab7e987b5acdb80301ac2e9cf18209
kelly883 <emekelechi883@gmail.com>
2022-11-11 02:43:15 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,33 @@
-#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * string_nconcat - a function that concatenates two strings.
- *
- * @s1: first char
- * @s2: secound char
- * @n: unsigned int
- *
- * Return: If the function fails, it should return NULL
+ * string_nconcat - concatenates two strings
+ * @s1:stirng 1
+ * @s2:string 2
+ * @n: n bytesofstring 2
+ * Return: pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int x, y, z;
-	char *s;
+  char *ptr, *empt;
+  unsigned int i, k;
 
-	if (s1 == NULL)
-	{
-		x = 0;
-	}
-	else
-	{
-		for (x = 0; s1[x]; ++x)
-		;
-	}
-	if (s2 == NULL)
-	{
-		y = 0;
-	}
-	else
-	{
-		for (y = 0; s2[y]; ++y)
-		;
-	}
-	if (y > n)
-		y = n;
-	s = malloc(sizeof(char) * (x + y + 1));
-	if (s == NULL)
-		return (NULL);
-	for (z = 0; z < x; z++)
-		s[z] = s1[z];
-	for (z = 0; z < y; z++)
-		s[z + x] = s2[z];
-	s[x + y] = '\0';
-	return (s);
+  empt = "";
+  if (s1 == NULL)
+    s1 = empt;
+  if (s2 == NULL)
+    s2 = empt;
+  for (i = 0; s1[i] != '\0'; i++)
+    ;
+  ptr = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
+  if (ptr == NULL)
+    return (NULL);
+  for (i = 0; s1[i] != '\0'; i++)
+    ptr[i] = s1[i];
+  for (k = 0; s2[k] != '\0' && k < n; k++, i++)
+    ptr[i] = s2[k];
+  ptr[i] = '\0';
+  return (ptr);
 }
