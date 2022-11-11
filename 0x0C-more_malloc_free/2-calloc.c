fd5f0608d495dd2bbb00d9b1d56c691f03128548
kelly883 <emekelechi883@gmail.com>
2022-11-11 02:54:01 -0800

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,25 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _calloc - allocates memory for an array of @nmemb elements of
- * @size bytes each and returns a pointer to the allocated memory.
- *
- * @nmemb: allocate memory for array
- * @size: allocate element of size bytes
- *
- * Return: pointer to the allocated memory.
+ *_calloc - allocate memory for an array usingmalloc
+ * @nmemb: number ofelements
+ * @size: size of bytes
+ * Return: pointer
  */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *a;
-	unsigned int b;
+  void *ptr;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	a = malloc(nmemb * size);
-	if (a == NULL)
-		return (NULL);
-	for (b = 0; b < (nmemb * size); b++)
-		a[b] = 0;
-	return (a);
+  if (nmemb == 0 || size == 0)
+    return (NULL);
+  ptr = malloc(nmemb * size);
+  if (ptr == NULL)
+    return (NULL);
+  _memset(ptr, 0, (nmemb * size));
+  return (ptr);
+}
+
+/**
+ *_memset - sets first n bytes of the memory area
+ * @s: array to set
+ * @b: what to set it to
+ * @n: first n bytes
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  unsigned int i;
+
+  for (i = 0; i < n; i++)
+    {
+      s[i] = b;
+    }
+  return (s);
 }
