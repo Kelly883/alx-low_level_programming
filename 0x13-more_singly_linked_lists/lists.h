cd6677aa2e69e8ab077046a1db688ed2de19e343
kelly883 <emekelechi883@gmail.com>
2022-11-29 12:06:40 -0800

    my lists.h file was the issue

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 int _putchar(char b);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -15,4 +15,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
+#endif
