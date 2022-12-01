79d933f31c614c9ce8f942480245681de0bbf5e7
kelly883 <emekelechi883@gmail.com>
2022-12-01 12:51:57 -0800

    recommited

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,7 @@ int get_endianness(void);
 
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int binary_to_uint(const char *b);int _putchar(char b);
+unsigned int binary_to_uint(const char *b);
+int _putchar(char b);
+#endif
+;
